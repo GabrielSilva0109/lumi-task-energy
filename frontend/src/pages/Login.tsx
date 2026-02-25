4098ecf72bb1ca2263726083de91036b51bc1ce8
Gabriel <silvagabriel0109@gmail.com>
2026-02-25 09:07:09 -0300

    retire front

--- frontend/src/pages/Login.tsx
+++
@@ -1,9 +0,0 @@
-
-
-export default function Login() {
-  return (
-    <div className="flex items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold">Login & Cadastro</h1>
-    </div>
-  )
-}
